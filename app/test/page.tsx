daf4efdbf5d5c8276701284a363fcc10efa0e157
Jack Lewicki <jacklewicki@tamu.edu>
2025-03-28 21:38:27 -0500

    Added cart context and an example of use in the test directory.

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,52 @@
+'use client'
+import { useCart, Item, Ingredient} from '@/components/cartContext'
+import Link from 'next/link';
+// To use the context, need the following three things
+// 'use client'
+// import { useCart, Item, Ingredient} from '@/components/cartContext'
+// const {cart, addToCart, removeFromCart}  = useCart()
+// Top 2 should be at beginning of file. Last one should be inside the export default function
+
 export default function Test() {
+  const {cart, addToCart, removeFromCart}  = useCart();
+
+  // Made up item just for testing
+  const itemToAdd:Item = {
+    name: "Classic Tea",
+    id: 1,
+    quantity: 1,
+    ingredients: [{
+      name:"Tea Leaves",
+      id:2
+    }]
+  }
+
+  const handleAddToCart = () => {    
+    addToCart(itemToAdd)
+  };
+
+  const handleRemoveFromCart = () => {
+    removeFromCart(0); // Removes index 0, which is the first element in the cart
+  }
+
+  function printLength(): string{
+    if(cart.length>0)
+    {
+        console.log(cart)
+        return ""+cart.length;
+    }
+    else
+    {
+      return "nothing in cart";
+    }
+  }
+
   return (
     <div>
-      <h1>Test</h1>
+      <h2 id="header2">{printLength()}</h2>
+      <button onClick={() => handleAddToCart()}>Add to cart</button>
+      <button  onClick={() => handleRemoveFromCart()}>Remove from cart</button>
+      {/* <Link href="/test2">CLICK ME</Link> */}
     </div>
   );
 }
---
+++ components/cartContext.tsx
@@ -0,0 +1,58 @@
+"use client";
+import assert from 'assert';
+import React, {createContext, useState, useContext, ReactNode} from 'react';
+// To use the context, need the following three things
+// 'use client'
+// import { useCart, Item, Ingredient} from '@/components/cartContext'
+// const {cart, addToCart, removeFromCart}  = useCart()
+// Top 2 should be at beginning of file. Last one should be inside the export default function
+
+
+// Type Definitions for Item, Ingredient, and the cartContextType
+export interface Item {
+    name: string
+    id: number
+    quantity: number
+    ingredients: Ingredient[]
+}
+
+export interface Ingredient {
+    name: string
+    id: number
+}
+
+interface cartContextType {
+    cart: Item[]
+    addToCart: (item:Item) => void;
+    removeFromCart: (index:number) => void;
+}
+
+interface Props{
+  children?: ReactNode
+}
+
+export const CartContext = createContext<cartContextType>({cart:[], addToCart: (item:Item)=>{}, removeFromCart: (index:number)=>{}});
+
+export function CartProvider({children}:Props) {
+  // setup for cart variable, addToCart method, and removeFromCart method
+  const [cart, setCart] = useState<Item[]>([]);
+
+  function addToCart(item:Item) {
+    setCart(c => [...c, item]);
+  }
+
+  function removeFromCart(index:number) {
+    assert(index >=0 && index < cart.length, "Invalid index passed to removeFromCart method in components/cartContext.tsx. Index got: "+ index+". Length: "+cart.length);
+    setCart(c => c.slice(0, index).concat(c.slice(index+1)));
+  }
+
+  return (
+    <CartContext.Provider value={{cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
